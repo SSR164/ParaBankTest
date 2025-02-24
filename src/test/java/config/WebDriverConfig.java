8f017912cc769bc34df62ab8e1bfff7d12cd6a6a
Serg Sh. <vampir6307@gmail.com>
2025-02-24 09:27:55 +0300

    Owner2

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -3,6 +3,8 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
+        "system:properties",
+        "system:env",
         "classpath:${env}.properties",// Использует файл, указанный через переменную окружения "env"
         "classpath:local.properties"   // Файл по умолчанию, если "env" не передан или файл не найден
 })
