6dc6751fcad943570b8452bfe1d6d35d9b755f1b
Serg Sh. <vampir6307@gmail.com>
2025-03-19 21:19:53 +0300

    all2

--- src/main/java/utils/Login.java
+++ src/main/java/utils/Login.java
@@ -9,102 +9,102 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class Login {
-    private final SelenideElement loginPanel = $("#loginPanel");
-    private final SelenideElement firstName1 = $("[name='customer.firstName']");
-    private final SelenideElement lastName1 = $("[name='customer.lastName']");
-    private final SelenideElement address1 = $("[name='customer.address.street']");
-    private final SelenideElement city1 = $("[name='customer.address.city']");
-    private final SelenideElement state1 = $("[name='customer.address.state']");
-    private final SelenideElement zipCode1 = $("[name='customer.address.zipCode']");
-    private final SelenideElement phoneNumber1 = $("[name='customer.phoneNumber']");
-    private final SelenideElement ssn1 = $("[name='customer.ssn']");
-    private final SelenideElement username1 = $("[name='customer.username']");
-    private final SelenideElement password1 = $("[name='customer.password']");
-    private final SelenideElement confirm1 = $("[name='repeatedPassword']");
-    private final SelenideElement register1 = $("#customerForm").$(".button");
+    private final SelenideElement loginPaneRepeat = $("#loginPanel");
+    private final SelenideElement firstNameRepeat = $("[name='customer.firstName']");
+    private final SelenideElement lastNameRepeat = $("[name='customer.lastName']");
+    private final SelenideElement addressRepeat = $("[name='customer.address.street']");
+    private final SelenideElement cityRepeat = $("[name='customer.address.city']");
+    private final SelenideElement stateRepeat = $("[name='customer.address.state']");
+    private final SelenideElement zipCodeRepeat = $("[name='customer.address.zipCode']");
+    private final SelenideElement phoneNumberRepeat = $("[name='customer.phoneNumber']");
+    private final SelenideElement ssnRepeat = $("[name='customer.ssn']");
+    private final SelenideElement usernameRepeat = $("[name='customer.username']");
+    private final SelenideElement passwordRepeat = $("[name='customer.password']");
+    private final SelenideElement confirmRepeat = $("[name='repeatedPassword']");
+    private final SelenideElement registerRepeat = $("#customerForm").$(".button");
 
 
     @Step("Открыть страницу")
-    public Login openPage1() {
+    public Login openPageRepeat() {
         open("/parabank/index.htm");
         return this;
     }
 
     @Step("Нажать на кнопку LOG IN ")
-    public Login clickRegister1() {
-        loginPanel.$(byText("Register")).click();
+    public Login clickRegisterRepeat() {
+        loginPaneRepeat.$(byText("Register")).click();
         return this;
     }
 
     @Step("Заполнить поле FirstName")//
-    public Login setValueFirstName1(String firstNameCustomer) {
-        firstName1.setValue(firstNameCustomer);
+    public Login setValueFirstNameRepeat(String firstNameCustomer) {
+        firstNameRepeat.setValue(firstNameCustomer);
         return this;
     }
 
     @Step("Заполнить поле Last Name")
-    public Login setValueLastName1(String lastNameCustomer) {
-        lastName1.setValue(lastNameCustomer);
+    public Login setValueLastNameRepeat(String lastNameCustomer) {
+        lastNameRepeat.setValue(lastNameCustomer);
         return this;
     }
 
     @Step("Заполнить поле Address")
-    public Login setValueAddress1(String addressCustomer) {
-        address1.setValue(addressCustomer);
+    public Login setValueAddressRepeat(String addressCustomer) {
+        addressRepeat.setValue(addressCustomer);
         return this;
     }
 
     @Step("Заполнить поле City")
-    public Login setValueCity1(String cityCustomer) {
-        city1.setValue(cityCustomer);
+    public Login setValueCityRepeat(String cityCustomer) {
+        cityRepeat.setValue(cityCustomer);
         return this;
     }
 
     @Step("Заполнить поле State")
-    public Login setValueState1(String stateCustomer) {
-        state1.setValue(stateCustomer);
+    public Login setValueStateRepeat(String stateCustomer) {
+        stateRepeat.setValue(stateCustomer);
         return this;
     }
 
     @Step("Заполнить поле Zip Code")
-    public Login setValueZipCode1(String zipCodeCustomer) {
-        zipCode1.setValue(zipCodeCustomer);
+    public Login setValueZipCodeRepeat(String zipCodeCustomer) {
+        zipCodeRepeat.setValue(zipCodeCustomer);
         return this;
     }
 
     @Step("Заполнить поле Phone")
-    public Login setValuePhone1(String phoneCustomer) {
-        phoneNumber1.setValue(phoneCustomer);
+    public Login setValuePhoneRepeat(String phoneCustomer) {
+        phoneNumberRepeat.setValue(phoneCustomer);
         return this;
     }
 
     @Step("Заполнить поле SSN")
-    public Login setValueSSN1(String ssnCustomer) {
-        ssn1.setValue(ssnCustomer);
+    public Login setValueSSNRepeat(String ssnCustomer) {
+        ssnRepeat.setValue(ssnCustomer);
         return this;
     }
 
     @Step("Заполнить поле Username")
-    public Login setValueUserName1(String userNameCustomer) {
-        username1.setValue(userNameCustomer);
+    public Login setValueUserNameRepeat(String userNameCustomer) {
+        usernameRepeat.setValue(userNameCustomer);
         return this;
     }
 
     @Step("Заполнить поле Password")
-    public Login setValuePassword1(String passwordCustomer) {
-        password1.setValue(passwordCustomer);
+    public Login setValuePasswordRepeat(String passwordCustomer) {
+        passwordRepeat.setValue(passwordCustomer);
         return this;
     }
 
     @Step("Заполнить поле Confirm")
-    public Login setValueConfirm1(String passwordCustomer) {
-        confirm1.setValue(passwordCustomer);
+    public Login setValueConfirmRepeat(String passwordCustomer) {
+        confirmRepeat.setValue(passwordCustomer);
         return this;
     }
 
     @Step("Нажать на кнопку Register")
     public Login clickRegister() {
-        register1.click();
+        registerRepeat.click();
         return this;
 
     }
